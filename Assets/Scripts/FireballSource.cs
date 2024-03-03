6578aa7f418e9e079c21b286f015497cdf7c3e5e
Blezz-tech <72557631+Blezz-tech@users.noreply.github.com>
2024-03-03 14:00:42 +0300

    step 20

--- Assets/Scripts/FireballSource.cs
+++ Assets/Scripts/FireballSource.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FireballSource : MonoBehaviour
 {
     public Transform targetPoint;
+    public Camera cameraLink;
+    public float targetInSkyDestance;
 
     void Start()
     {
@@ -13,6 +15,18 @@ public class FireballSource : MonoBehaviour
 
     void Update()
     {
+        var ray = cameraLink.ViewportPointToRay(new Vector3(0.5f, 0.7f, 0));
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint.position = hit.point;
+        }
+        else
+        {
+            targetPoint.position = ray.GetPoint(targetInSkyDestance);
+        }
+
         transform.LookAt(targetPoint.position);
     }
 }
