95b0bb5a078ae4e978fb623cf776a27d2807f8cf
Blezz-tech <72557631+Blezz-tech@users.noreply.github.com>
2024-03-03 13:06:13 +0300

    step 16

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class Fireball : MonoBehaviour
 {
     public float speed;
+    public float lifetime;
+
+    private void Start()
+    {
+        Invoke("DestroyFireball", lifetime);
+    }
 
     void FixedUpdate()
     {
         transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DestroyFireball();
+    }
+
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
     }
 }
