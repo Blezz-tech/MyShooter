ca3cca06502d9bd338872572d9c9e2c2b1005ca2
Blezz-tech <72557631+Blezz-tech@users.noreply.github.com>
2024-03-03 13:27:23 +0300

    step 18

--- Assets/Scripts/FireballCaster.cs
+++ Assets/Scripts/FireballCaster.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class FireballCaster : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Fireball fireballPrefab;
+    public Transform fireballSourceTransform;
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, fireballSourceTransform.position, fireballSourceTransform.rotation);
+        }
     }
 }
