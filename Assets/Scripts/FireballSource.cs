71804c61415fc36412982314f726ff6e00426f03
Blezz-tech <72557631+Blezz-tech@users.noreply.github.com>
2024-03-03 13:47:18 +0300

    step 19

---
+++ Assets/Scripts/FireballSource.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballSource : MonoBehaviour
+{
+    public Transform targetPoint;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.LookAt(targetPoint.position);
+    }
+}
