0e626f492277644ca7d18aba7556a30d9abdc5e2
Blezz-tech <72557631+Blezz-tech@users.noreply.github.com>
2024-03-03 12:56:28 +0300

    step 15

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+
+    void FixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
