fcd6f73c68f90a6ffaf0bba87c3201eff6e3bfb8
Blezz-tech <72557631+Blezz-tech@users.noreply.github.com>
2024-03-26 20:52:20 +0300

    step 4

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float maxSize = 5;
+    public float speed = 1;
+
+    void Start()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    void Update()
+    {
+        transform.localScale += Vector3.one * Time.deltaTime * speed;
+
+        if(transform.localScale.x > maxSize)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
